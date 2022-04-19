0ff5eaa9d1fa04be9a43062da772cf7a13ffe78c
Brad Breiten <jonathan@breiten.us>
2022-04-19 10:44:39 -0600

    Linted code

--- app/controllers/api/v1/teas_controller.rb
+++ app/controllers/api/v1/teas_controller.rb
@@ -1,13 +1,18 @@
-class Api::V1::TeasController < ApplicationController
-  def create
-    tea = Tea.find_or_create_by!(tea_params)
-    render json: TeaSerializer.new(tea), status: 201
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class TeasController < ApplicationController
+      def create
+        tea = Tea.find_or_create_by!(tea_params)
+        render json: TeaSerializer.new(tea), status: 201
+      end
+
+      private
+
+      def tea_params
+        params.permit(:title, :description, :temperature, :brew_time)
+      end
+    end
   end
-
-  private
-
-  def tea_params
-    params.permit(:title, :description, :temperature, :brew_time)
-  end
-
 end
